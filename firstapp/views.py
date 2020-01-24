dc23aeca57a3513145fe83ffc985190e324ea6b9
Shiva Satapathy <shivasatapathy.appstone@gmail.com>
2020-01-24 12:29:15 +0530

    2nd django class

--- firstapp/views.py
+++ firstapp/views.py
@@ -4,5 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("<h1>Hello Aditi</h1>")
-
+   return render(request,'index.html')
+def add(request):
+    n1=request.GET['num1']
+    n2=request.GET['num2']
+    sum=int(n1)+int(n2)
+    return render(request,'result.html',{"result":sum})
