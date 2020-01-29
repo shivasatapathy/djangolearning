e1c18bfcb1759c7c36a7ad7409a2396d168291f2
Shiva Satapathy <shivasatapathy.appstone@gmail.com>
2020-01-29 10:05:01 +0530

    3rd class

--- firstapp/views.py
+++ firstapp/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 def home(request):
    return render(request,'index.html')
 def add(request):
-    n1=request.GET['num1']
-    n2=request.GET['num2']
+    n1=request.POST['num1']
+    n2=request.POST['num2']
     sum=int(n1)+int(n2)
     return render(request,'result.html',{"result":sum})
